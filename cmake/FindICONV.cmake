48f822ec56f81165560a26ad689e5a65694a8e05
NikitaFeodonit <nikitafeodonit@yandex.com>
2017-01-17 13:07:29 +0300

    Update borsch scripts

--- cmake/FindICONV.cmake
+++ cmake/FindICONV.cmake
@@ -19,14 +19,14 @@
 # You should have received a copy of the GNU General Public License
 # along with this script.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
-# - Try to find Iconv 
-# Once done this will define 
-# 
-#  ICONV_FOUND - system has Iconv 
-#  ICONV_INCLUDE_DIR - the Iconv include directory 
-#  ICONV_LIBRARIES - Link these to use Iconv 
+# - Try to find Iconv
+# Once done this will define
+#
+#  ICONV_FOUND - system has Iconv
+#  ICONV_INCLUDE_DIR - the Iconv include directory
+#  ICONV_LIBRARIES - Link these to use Iconv
 #  ICONV_SECOND_ARGUMENT_IS_CONST - the second argument for iconv() is const
-# 
+#
 include(CheckCCompilerFlag)
 include(CheckCSourceCompiles)
 
@@ -53,48 +53,43 @@ find_path(ICONV_INCLUDE_DIR iconv.h PATHS /opt/local/include /sw/include /usr/in
 
 string(REGEX REPLACE "(.*)/include/?" "\\1" ICONV_INCLUDE_BASE_DIR "${ICONV_INCLUDE_DIR}")
 
-find_library(ICONV_LIBRARIES NAMES libiconv c libc iconv libiconv.lib PATHS /usr/lib /usr/local/lib /usr/lib/x86_64-linux-gnu)
+find_library(ICONV_LIBRARIES NAMES libiconv iconv libiconv.lib libiconv.dylib)
 
-if(ICONV_INCLUDE_DIR AND ICONV_LIBRARIES) 
-   set(ICONV_FOUND TRUE) 
-endif() 
-
-set(CMAKE_REQUIRED_INCLUDES ${ICONV_INCLUDE_DIR})
-set(CMAKE_REQUIRED_LIBRARIES ${ICONV_LIBRARIES})
-
-if(ICONV_FOUND)
-  check_c_compiler_flag("-Werror" ICONV_HAVE_WERROR)
-  set (CMAKE_C_FLAGS_BACKUP "${CMAKE_C_FLAGS}")
-  if(ICONV_HAVE_WERROR)
-    set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Werror")
-  endif(ICONV_HAVE_WERROR)
-  check_c_source_compiles("
-  #include <iconv.h>
-  int main(){
-    iconv_t conv = 0;
-    const char* in = 0;
-    size_t ilen = 0;
-    char* out = 0;
-    size_t olen = 0;
-    iconv(conv, &in, &ilen, &out, &olen);
-    return 0;
-  }
-  " ICONV_SECOND_ARGUMENT_IS_CONST )
-  set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS_BACKUP}")
+if(NOT ICONV_LIBRARIES AND UNIX)
+    find_library(ICONV_LIBRARIES NAMES c libc)
 endif()
 
-set(CMAKE_REQUIRED_INCLUDES)
-set(CMAKE_REQUIRED_LIBRARIES)
+if(ICONV_INCLUDE_DIR AND ICONV_LIBRARIES)
+    set(CMAKE_REQUIRED_INCLUDES ${ICONV_INCLUDE_DIR})
+    set(CMAKE_REQUIRED_LIBRARIES ${ICONV_LIBRARIES})
+    check_c_compiler_flag("-Werror" ICONV_HAVE_WERROR)
+    set (CMAKE_C_FLAGS_BACKUP "${CMAKE_C_FLAGS}")
+    if(ICONV_HAVE_WERROR)
+    set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Werror")
+    endif(ICONV_HAVE_WERROR)
+    check_c_source_compiles("
+        #include <iconv.h>
+        int main(){
+        iconv_t conv = 0;
+        const char* in = 0;
+        size_t ilen = 0;
+        char* out = 0;
+        size_t olen = 0;
+        iconv(conv, &in, &ilen, &out, &olen);
+        return 0;
+        }
+    " ICONV_SECOND_ARGUMENT_IS_CONST )
+    set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS_BACKUP}")
+endif()
 
-if(ICONV_FOUND) 
-  if(NOT ICONV_FIND_QUIETLY) 
-    message(STATUS "Found Iconv: ${ICONV_LIBRARIES}") 
-  endif(NOT ICONV_FIND_QUIETLY) 
-else() 
-  if(Iconv_FIND_REQUIRED) 
-    message(FATAL_ERROR "Could not find Iconv") 
-  endif(Iconv_FIND_REQUIRED) 
-endif() 
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(ICONV DEFAULT_MSG ICONV_INCLUDE_DIR ICONV_LIBRARIES)
+
+# Copy the results to the output variables.
+if(ICONV_FOUND)
+  set(ICONV_LIBRARY ${ICONV_LIBRARIES})
+  set(ICONV_INCLUDE_DIRS ${ICONV_INCLUDE_DIR})
+endif()
 
 mark_as_advanced(
     ICONV_INCLUDE_DIR
